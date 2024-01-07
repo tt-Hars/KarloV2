14115b738a2e40e2b7f790dbe0bdc0fff84df383
h.t <h@t.co>
2024-01-07 16:48:01 +0530

    fixed build failure

--- modules/shared/ui/src/index.ts
+++ modules/shared/ui/src/index.ts
@@ -4,4 +4,3 @@ export * from './lib/components/tile-card/tile-card';
 export * from './lib/components/navbar/navbar';
 export * from './lib/components/main-heading/main-heading';
 export * from './lib/components/backdrop-loader/backdrop-loader';
-export * from '../../data/src/lib/constants/theme';
