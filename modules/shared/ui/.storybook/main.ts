f40da54f87bd00754a75d20e30e9e9f2b66efbda
h.t. <h.t@t.co>
2025-12-07 21:56:12 +0530

    chore: migrate nx to 22.1.3

--- modules/shared/ui/.storybook/main.ts
+++ modules/shared/ui/.storybook/main.ts
@@ -1,12 +1,18 @@
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
   stories: ['../src/lib/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [getAbsolutePath("@storybook/addon-docs")],
   framework: {
-    name: '@storybook/react-webpack5',
+    name: getAbsolutePath("@storybook/react-webpack5"),
     options: {},
   },
 };
 
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
+}
